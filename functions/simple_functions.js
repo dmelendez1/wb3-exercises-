84f391863dab54139ebd9f9ea24942a201d877fc
melzmatz <melendezdiana95@gmail.com>
2023-10-24 13:30:36 -0700

    simplefunctions

---
+++ functions/simple_functions.js
@@ -0,0 +1,19 @@
+function listFavoriteThings() {
+console.log("My name is Diana Melendez")
+console.log("My favorite movie is Interstellar")
+console.log("My favorite artist is The Weeknd")
+}
+listFavoriteThings()
+
+
+function whyImHere() {
+    console.log("I joined Learning to code program because I want a job I actaully love")
+}
+
+whyImHere()
+
+function favoritePlace() {
+    console.log("My favorite place is my home")
+}
+
+favoritePlace()
