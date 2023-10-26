fe20576e65bb3a4fe2bc158e187bf8e5000ace70
melzmatz <melendezdiana95@gmail.com>
2023-10-26 10:25:39 -0700

    test&exercies

--- Strings/advanced_parsing.js
+++ Strings/advanced_parsing.js
@@ -1,15 +1,28 @@
-function removeFirstName(fullNmae) {
-    let 
+function hasMiddleName(fullName) {
+    let index = fullName.indexOf(" ");
+    let lastIndex = fullName.lastIndexOf(" ");
+    
+    return index > 0 && (lastIndex > index);
 }
 
-function parseAndDisplayName(fullName) {
-    let end = fullName.indexOf(" ");
-    let firstName = fullName.substring(0, end);
-    console.log(`First Name:`, firstName);
 
-    let lastName = fullName.substring(end + 1);
-    console.log(`Last Name:`, lastName);
+function getFirstName(fullname) {
+    fullName = fullName.trim()
+    let index = fullName.indexOf(" ");
+
+    if(index < 0) return fullName;
+
+    return fullName.substring(0, index)
 }
+
+function parseAndDisplay(fullName) {
+    if(hasMiddleNmae(fullName)) {
+        console.log("Middle Name:", hasMiddleName(fullName));
+    } else {
+        console.log("First Name:", getFirstName(fullName));
+    }
+}
+
 
 parseAndDisplayName("Cher");
 parseAndDisplayName("Brenda Kaye");
